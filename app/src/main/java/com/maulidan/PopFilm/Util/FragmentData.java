e60471aa12c8c81c36ebee973a7692b5ded9e12c
maulani14 <anismaulani06@gmail.com>
2019-05-26 14:49:25 +0700

    film

---
+++ app/src/main/java/com/maulidan/PopFilm/Util/FragmentData.java
@@ -0,0 +1,10 @@
+package com.maulidan.PopFilm.Util;
+
+import android.database.Cursor;
+
+/**
+ * Created by bajau on 5/9/16.
+ */
+public interface FragmentData {
+    public void updateData(Cursor movie, String sort);
+}
